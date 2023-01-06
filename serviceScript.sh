2191c4a25ca0bf0beb612df7e41153699c3c480d
NGOSSO EBENE Adolphe Kevin <ngossoebene@et.esiea.fr>
2023-01-06 19:13:09 +0100

    creation de fichier de deploiement et de services pour passage sur worker

---
+++ serviceScript.sh
@@ -0,0 +1,11 @@
+
+
+kubectl delete svc frontend 
+kubectl delete svc backend-feed
+kubectl delete svc backend-user
+kubectl delete svc reverseproxy
+
+kubectl apply -f feed-service.yaml
+kubectl apply -f frontend-service.yaml
+kubectl apply -f reverseproxy-service.yaml
+kubectl apply -f user-service.yaml
